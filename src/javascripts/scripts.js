3c3f9fcd9001bf9e9cb60d01477a500b2b025fc3
Declan Van Welie <declanv@gmail.com>
2021-04-06 21:59:56 -0400

    Adding: basic styling for poem section

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,1 +1,10 @@
 // Add your scripts here
+
+const stanzas = document.querySelectorAll('#poem h1');
+
+for (let i = 1; i < stanzas.length; i += 1) {
+  setTimeout(() => {
+    stanzas[i - 1].classList.remove('show');
+    stanzas[i].classList.add('show');
+  }, 3000 * i);
+}
